f21eaecac74884f069334cc30f979d944a1810ed
Neville Brem <bremneville@gmail.com>
2024-09-09 18:17:38 +0200

    init + first few steps

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<div class="flex items-center flex-col my-16">
+	<h1 class="text-5xl mb-8 font-black uppercase tracking-tight">Zap it!</h1>
+	<form class="flex gap-4">
+		<input type="file" class="file-input file-input-bordered file-input-primary w-full max-w-xs" />
+		<select name="option" class="select select-primary" id="">
+			<option value="0" disabled>Select your output</option>
+			<option value="quizlet">Quizlet</option>
+			<option value="json">JSON</option>
+			<option value="remnote">Remnote</option>
+		</select>
+	</form>
+</div>
---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import daisyui from 'daisyui';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,svelte,ts,js}'],
+	theme: {
+		extend: {}
+	},
+	plugins: [daisyui],
+	daisyui: {
+		themes: ['sunset']
+	}
+};
